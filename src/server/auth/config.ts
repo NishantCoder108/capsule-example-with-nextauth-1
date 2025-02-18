b6464764ae17b52d51a534f66d3d74d1fcfdcbf5
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-02-18 19:10:13 +0530

    add console to see data flow

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -38,9 +38,13 @@ export const authConfig = {
       if (user) {
         token.userId = user.id;
       }
+
+      console.log("jwt 4", token);
       return token;
     },
     async session({ session, token }) {
+      console.log("session 6", session);
+      console.log("session token 7", token);
       const dbuser = await db.user.findUnique({
         where: { id: token.userId as string },
       });
@@ -49,9 +53,12 @@ export const authConfig = {
         session.user.id = token.userId as string;
         session.user.email = email as string;
       }
+
+      console.log("session 5", session);
       return session;
     },
     signIn: async ({ user }) => {
+      console.log("signIn 8", user);
       return true;
     },
   },
@@ -66,10 +73,14 @@ export const authConfig = {
         serializedSession: { label: "Serialized Session", type: "text" },
       },
       async authorize(credentials) {
+        console.log("authorize 1", credentials);
         if (!credentials?.userId) {
           throw new Error("No user ID provided");
         }
-        capsuleServer.importSession(credentials.serializedSession as string);
+        const d = capsuleServer.importSession(
+          credentials.serializedSession as string,
+        );
+        console.log("capsuleServer.importSession 2", d);
         try {
           const user = await db.user.upsert({
             where: { id: credentials.userId as string },
@@ -80,6 +91,7 @@ export const authConfig = {
             },
           });
 
+          console.log("authorize 3", user);
           return user;
         } catch (error) {
           console.error("Authorize error:", error);
@@ -91,6 +103,7 @@ export const authConfig = {
   // Add a custom page that will handle the Capsule modal
   pages: {
     signIn: "/auth/signin",
+    signOut: "/auth/logout",
   },
   session: {
     strategy: "jwt", // Use JWT strategy for sessions
